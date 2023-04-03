52fd9b6ec9a99256920d3ca20866d3cffa8f0712
wuyang <ocean5tech@hotmail.com>
2023-04-04 00:45:59 +0800

    连接层Readme更新

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -12,8 +12,8 @@ func TestConnect(t *testing.T) {
 
 	tra.Connect(trb)
 	trb.Connect(tra)
-	assert.Equal(t, tra.peers[trb.addr], trb)
-	assert.Equal(t, trb.peers[tra.addr], tra)
+	assert.Equal(t, tra.peers[trb.Addr()], trb)
+	assert.Equal(t, trb.peers[tra.Addr()], tra)
 }
 
 func TestSendMessage(t *testing.T) {
@@ -29,4 +29,20 @@ func TestSendMessage(t *testing.T) {
 	rpc := <-trb.Consume()
 	assert.Equal(t, rpc.Payload, msg)
 	assert.Equal(t, rpc.From, tra.addr)
+}
+
+func TestSendMessageServer(t *testing.T){
+	aRemote := network.NewLocalTransport("A") //注册
+	bRemote := network.NewLocalTransport("B") //注册
+	cRemote := network.NewLocalTransport("C") //不注册
+	dRemote := network.NewLocalTransport("D") //不注册
+	aRemote.Connect(bRemote) // A-B,都在Server中注册
+	bRemote.Connect(aRemote)
+	aRemote.Connect(cRemote) // A-C, A注册，C不注册
+	cRemote.Connect(aRemote)
+	cRemote.Connect(dRemote) // C-D, C不注册，d不注册
+	dRemote.Connect(cRemote)
+	bRemote.Connect(dRemote) // B-> D 单项
+
+	
 }
